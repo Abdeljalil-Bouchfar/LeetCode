df785ec2384bd69240c192f5209f026ca30712bb
Abdeljalil Bouchfar <55533328+Abdeljalil-Bouchfar@users.noreply.github.com>
2022-10-03 18:37:17 +0100

    Time: 0 ms (100.00%), Space: 6.1 MB (92.26%) - LeetHub

---
+++ 91-decode-ways/91-decode-ways.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        if (s[0] == '0')
+            return 0;
+        int n = 1, f1 = 1, f2 = 1;
+        for (int i = 1; i < s.length(); i++)
+        {
+            if (s[i] == '0' && (s[i - 1] > '2' || s[i - 1] == '0'))
+                return 0;
+            if(((s[i] != '0' && s[i - 1] == '1') || (s[i] != '0' && s[i - 1] == '2' && s[i] <= '6')) && s[i + 1] != '0')
+            {
+                f2 += f1;
+                f1 = f2 - f1;
+            }
+            else if (f2 != 1)
+            {
+                n *= f2;
+                f2 = f1 = 1;
+            }
+            
+        }
+       if (f2 != 1)
+            n *= f2;
+        return n;
+    }
+};
