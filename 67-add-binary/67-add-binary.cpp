43c13f4bc1012a66fd92f0fcd3cc8539d88635fa
Abdeljalil Bouchfar <55533328+Abdeljalil-Bouchfar@users.noreply.github.com>
2022-10-09 12:36:08 +0100

    Time: 3 ms (80.23%), Space: 6.3 MB (85.28%) - LeetHub

--- 67-add-binary/67-add-binary.cpp
+++ 67-add-binary/67-add-binary.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        string res = "";
+        // first of all adding binary numbers it's exactly like adding decimal numbers
+        // You just need to remaber that you're restrected to only 0 and 1 digit.
+        // as an example 1 + 1 in decimal is 2 but in binary it's 10, so we but 0 and carry 1.
+        string res;
         
         int carry = 0, i = a.size(), j = b.size();
         while(i > 0 || j > 0 || carry == 1)
         {
+            // Here we save carry by subtract by '0' to convert the numbers from a char type into an int (ASCII)
             if (i > 0) carry += a[--i] - '0';
             if (j > 0) carry += b[--j] - '0';
-            res = char(carry % 2 + '0') + res;
+            // Here we add the 
+            res += char(carry % 2 + '0');
             carry /= 2;
         }
-        
+        reverse(res.begin(), res.end());
         return res;
     }
 };
