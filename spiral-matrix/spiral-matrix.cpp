276e5f8046a6056f8a76355129744d467290d1ec
Abdeljalil Bouchfar <55533328+Abdeljalil-Bouchfar@users.noreply.github.com>
2023-05-09 10:41:14 +0000

    Time: 5 ms (33.67%) | Memory: 7 MB (30.23%) - LeetSync

--- spiral-matrix/spiral-matrix.cpp
+++ spiral-matrix/spiral-matrix.cpp
@@ -2,21 +2,20 @@ class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         int xs = 0, xe = matrix[0].size() - 1,  ys = 0, ye = matrix.size() - 1;
+        int t = matrix[0].size() * matrix.size();
         vector<int> res;
-        while(res.size() < matrix[0].size() * matrix.size())
+        while(res.size() < t)
         {
-            for (int i = xs; i <= xe; i++) 
+            for (int i = xs; i <= xe && res.size() < t; i++) 
                 res.push_back(matrix[ys][i]);
-            for (int i = ys + 1; i <= ye; i++) 
+            for (int i = ys + 1; i <= ye && res.size() < t; i++) 
                 res.push_back(matrix[i][xe]);
-            for (int i = xe - 1; i >= xs; i--) 
+            for (int i = xe - 1; i >= xs && res.size() < t; i--) 
                 res.push_back(matrix[ye][i]);
-            for (int i = ye - 1; i > ys; i--) 
+            for (int i = ye - 1; i > ys && res.size() < t; i--) 
                 res.push_back(matrix[i][xs]);
             xs++, ys++, xe--, ye--;
         }
-        while(res.size() > matrix[0].size() * matrix.size())
-            res.pop_back();
         return res;
     }
 };
