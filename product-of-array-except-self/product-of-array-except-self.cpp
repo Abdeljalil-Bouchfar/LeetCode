14fc6f22c3f942bd57d6badb1266179634c1ea67
Abdeljalil Bouchfar <55533328+Abdeljalil-Bouchfar@users.noreply.github.com>
2023-05-21 15:49:23 +0100

    Time: 23 ms (84.04%) | Memory: 25.1 MB (30.66%) - LeetSync

--- product-of-array-except-self/product-of-array-except-self.cpp
+++ product-of-array-except-self/product-of-array-except-self.cpp
@@ -4,13 +4,14 @@ public:
         vector<int> pre(nums.size(), nums[0]);
         vector<int> suf(nums.size(), nums[nums.size() - 1]);
         vector<int> ans(nums.size());
-        
         for(int i = 1; i < nums.size(); i++)
+        {
             pre[i] = nums[i] * pre[i - 1];
-
+        }
         for(int i = nums.size() - 2; i >= 0; i--)
+        {
             suf[i] = nums[i] * suf[i + 1];
-
+        }
         for(int i = 0; i < nums.size(); i++)
         {
             if (i == 0)
