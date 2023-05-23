872e8fee06e3e4092b4c735f68ecd5a9ca317366
Abdeljalil Bouchfar <55533328+Abdeljalil-Bouchfar@users.noreply.github.com>
2023-05-23 22:54:15 +0100

    Time: 20 ms (79.95%) | Memory: 17.9 MB (95.06%) - LeetSync

--- valid-sudoku/valid-sudoku.cpp
+++ valid-sudoku/valid-sudoku.cpp
@@ -4,27 +4,33 @@ public:
         for (int r = 0; r < 9; r++)
         {
             int row[10] = {0};
+            int column[10] = {0};
             for (int c = 0; c < 9; c++)
             {
                 if (board[r][c] != '.')
                     row[board[r][c] - '0']++;
-            }
-            for (int i = 0; i < 10; i++)
-                if (row[i] > 1)
-                    return false;
-        }
-        for (int r = 0; r < 9; r++)
-        {
-            int column[10] = {0};
-            for (int c = 0; c < 9; c++)
-            {
                 if (board[c][r] != '.')
                     column[board[c][r] - '0']++;
             }
             for (int i = 0; i < 10; i++)
-                if (column[i] > 1)
+            {
+                if (row[i] > 1 || column[i] > 1)
                     return false;
+            }
         }
+
+        // for (int r = 0; r < 9; r++)
+        // {
+        //     int column[10] = {0};
+        //     for (int c = 0; c < 9; c++)
+        //     {
+        //         if (board[c][r] != '.')
+        //             column[board[c][r] - '0']++;
+        //     }
+        //     for (int i = 0; i < 10; i++)
+        //         if (column[i] > 1)
+        //             return false;
+        // }
         for (int r = 3; r <= 9; r+=3)
         {
             for (int c = 3; c <= 9; c+=3)
