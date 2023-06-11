5eed98115d80f72eac465dce4ac8c12b904342c3
Abdeljalil Bouchfar <55533328+Abdeljalil-Bouchfar@users.noreply.github.com>
2023-06-11 21:51:53 +0000

    Time: 3 ms (36.01%) | Memory: 6 MB (74.13%) - LeetSync

--- climbing-stairs/climbing-stairs.cpp
+++ climbing-stairs/climbing-stairs.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        long o = 1, t = 1, ans = 1;
+        long n1 = 1, n2 = 1, ans = 1;
         for (int i = 1; i < n; i++)
-            ans = o + t, o = t, t = ans;
+            ans = n1 + n2, n1 = n2, n2 = ans;
         return ans;
     }
 };
