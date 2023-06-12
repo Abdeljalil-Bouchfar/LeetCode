1944c96168f9254bf9f9d2611f20911114e1e949
Abdeljalil Bouchfar <55533328+Abdeljalil-Bouchfar@users.noreply.github.com>
2023-06-12 08:42:51 +0000

    Time: 0 ms (100.00%) | Memory: 7 MB (13.50%) - LeetSync

--- summary-ranges/summary-ranges.cpp
+++ summary-ranges/summary-ranges.cpp
@@ -6,7 +6,9 @@ public:
         for (int i = 0; i < nums.size(); i++)
         {
             if (i + 1 >= nums.size() || nums[i + 1] != nums[i] + 1)
+            {
                 ans.push_back(pre + to_string(nums[i])), pre = "";
+            }
             else if (pre.empty())
                 pre = to_string(nums[i]) + "->";
         }
