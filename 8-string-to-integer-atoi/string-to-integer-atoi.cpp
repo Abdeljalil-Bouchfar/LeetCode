f5a42721f0efe3f2697686f464b54de48dd04ff3
Abdeljalil Bouchfar <55533328+Abdeljalil-Bouchfar@users.noreply.github.com>
2023-07-15 12:05:37 +0100

    Time: 4 ms (62.65%) | Memory: 6.9 MB (99.88%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -4,23 +4,19 @@ public:
         long long n = 0;
         int sign = 1, i = 0;
 
-	    while (s[i] == 32 || (s[i] >= 9 && s[i] <= 13))
-	    	i++;
+	    while (i < s.size() && s[i] == 32)
+            i++;
         
-	    if (s[i] == '-' || s[i] == '+')
-	    {
-	    	if (s[i] == '-')
-	    		sign = -1;
-	    	i++;
-	    }
+	    if (s[i] == '-')
+	    	sign = -1;
+	    i += (s[i] == '-' || s[i] == '+');
 
 	    for (; i < s.size() && s[i] >= '0' && s[i] <= '9'; i++)
 	    {
 	    	n = n * 10 + (s[i] - 48);
-            if (n * sign >= INT_MAX)
-                return 2147483647;
-            else if (n * sign <= INT_MIN)
-                return -2147483648;
+            
+            if (n > INT_MAX)
+                return sign == 1 ? 2147483647 : -2147483648;
 	    }
 	    return (n * sign);
     }
