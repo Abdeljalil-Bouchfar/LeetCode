f5e936ce5f5d1456b3f157ec91ed67b3f14527b5
Abdeljalil Bouchfar <55533328+Abdeljalil-Bouchfar@users.noreply.github.com>
2023-07-25 10:41:28 +0100

    Time: 4 ms (36.17%) | Memory: 6.4 MB (66.53%) - LeetSync

---
+++ 394-decode-string/decode-string.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        string ans;
+        int nb = 0;
+        stack<pair<int, string>> st;
+        for (char &c : s)
+        {
+            if (isdigit(c))
+                nb = (nb * 10) + (c - '0');
+            else if (isalpha(c))
+                ans += c;
+            else if (c == '[')
+                st.push({nb, ans}), ans = "", nb = 0;
+            else if (c == ']')
+            {
+                string tmp = ans;
+                while (--st.top().first)
+                    tmp += ans;
+                ans = st.top().second + tmp;
+                st.pop();
+            }
+        }
+        return ans;
+    }
+};
+// 3, ""
+// 2, "a"
+// tmp = c
