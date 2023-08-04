82468699070845cda3f1c1a955faf370a45fc7df
Abdeljalil Bouchfar <55533328+Abdeljalil-Bouchfar@users.noreply.github.com>
2023-08-04 17:10:00 +0000

    Time: 10 ms (89.79%) | Memory: 8.8 MB (61.76%) - LeetSync

---
+++ 383-ransom-note/ransom-note.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int hash[26] = {0};
+        for (char &c : magazine)
+            hash[c - 'a']++;
+        for (char &c : ransomNote)
+        {
+            if (hash[c - 'a'] == 0)
+                return false;
+            hash[c - 'a']--;
+        }
+        return true;
+    }
+};
