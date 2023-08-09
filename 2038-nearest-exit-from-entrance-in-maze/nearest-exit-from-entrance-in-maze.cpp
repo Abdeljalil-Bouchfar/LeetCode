ca3e3c74a04d68d64cea539fdeba7d3127e005c6
Abdeljalil Bouchfar <55533328+Abdeljalil-Bouchfar@users.noreply.github.com>
2023-08-09 15:05:17 +0000

    Time: 156 ms (33.51%) | Memory: 33.2 MB (49.53%) - LeetSync

---
+++ 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
+        int n = maze.size(), m = maze.front().size();
+        vector<vector<int>> visited(n, vector<int>(m, 0));
+        //vector<vector<int>> visited(maze.size(), vector<int>(maze.front().size(), 0));
+        const int dr[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        queue<pair<int, int>> qu;
+        qu.push({entrance[0], entrance[1]});
+        visited[entrance[0]][entrance[1]] = 1;
+        while (!qu.empty())
+        {
+            pair<int, int> p = qu.front();
+            cout << p.first << ' ' << p.second << endl;
+            qu.pop();
+            for (int i = 0; i < 4; i++)
+            {
+                int x = p.second + dr[i][0], y = p.first + dr[i][1];
+
+                if (x >= 0 && x < m && y >= 0 && y < n && !visited[y][x] && maze[y][x] == '.')
+                {
+                    qu.push({y, x}), visited[y][x] = visited[p.first][p.second] + 1;
+                    if (x == 0 || y == 0 || x + 1 == m || y + 1 == n)
+                        return visited[y][x] - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
