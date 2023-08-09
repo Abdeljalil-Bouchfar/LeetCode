0b5a8e6c251f6e43dae4cfad10fca35e63539775
Abdeljalil Bouchfar <55533328+Abdeljalil-Bouchfar@users.noreply.github.com>
2023-08-09 15:09:32 +0000

    Time: 161 ms (31.16%) | Memory: 33.4 MB (43.35%) - LeetSync

--- 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.cpp
+++ 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
         int n = maze.size(), m = maze.front().size();
-        vector<vector<int>> visited(n, vector<int>(m, 0));
-        //vector<vector<int>> visited(maze.size(), vector<int>(maze.front().size(), 0));
+        vector<vector<int>> visited(n, vector<int>(m, -1));
+
         const int dr[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
         queue<pair<int, int>> qu;
         qu.push({entrance[0], entrance[1]});
-        visited[entrance[0]][entrance[1]] = 1;
+        visited[entrance[0]][entrance[1]] = 0;
         while (!qu.empty())
         {
             pair<int, int> p = qu.front();
@@ -17,11 +17,11 @@ public:
             {
                 int x = p.second + dr[i][0], y = p.first + dr[i][1];
 
-                if (x >= 0 && x < m && y >= 0 && y < n && !visited[y][x] && maze[y][x] == '.')
+                if (x >= 0 && x < m && y >= 0 && y < n && visited[y][x] < 0 && maze[y][x] == '.')
                 {
                     qu.push({y, x}), visited[y][x] = visited[p.first][p.second] + 1;
                     if (x == 0 || y == 0 || x + 1 == m || y + 1 == n)
-                        return visited[y][x] - 1;
+                        return visited[y][x];
                 }
             }
         }
