1ab4bacbad054ed9a67b5ec891664b34ffdee120
Abdeljalil Bouchfar <55533328+Abdeljalil-Bouchfar@users.noreply.github.com>
2023-08-12 14:48:57 +0000

    Time: 3 ms (41.25%) | Memory: 6.4 MB (60.94%) - LeetSync

---
+++ 290-word-pattern/word-pattern.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        unordered_map<char, string> a;
+        unordered_map<string, char> p;
+        istringstream iss(s);
+        string word;
+        int i = 0;
+        while (iss >> word)
+        {
+            if (i == pattern.size() || (a.count(pattern[i]) && a[pattern[i]] != word) || (p.count(word) && p[word] != pattern[i]))
+                return false;
+            p[word] = pattern[i];
+            a[pattern[i++]] = word;
+        }
+        return i == pattern.size();
+    }
+};
