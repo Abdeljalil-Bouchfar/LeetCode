d66651d88340ce5a5380d78d7aa1d10ca0c73dd7
Abdeljalil Bouchfar <55533328+Abdeljalil-Bouchfar@users.noreply.github.com>
2023-08-13 09:57:03 +0000

    Time: 58 ms (94.88%) | Memory: 20.5 MB (84.49%) - LeetSync

---
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool closeStrings(string word1, string word2) {
+        if (word1.size() != word2.size()) return false;
+
+        int arr1[26] = {0};
+        int arr2[26] = {0};
+
+        for (int i = 0; i < word1.size(); i++)
+            arr1[word1[i] - 'a']++, arr2[word2[i] - 'a']++;
+        
+
+        for (int i = 0; i < word1.size(); i++)
+            if (arr2[word1[i] - 'a'] == 0 || arr1[word2[i] - 'a'] == 0)
+                return false;
+
+        sort(arr1, arr1 + 26);
+        sort(arr2, arr2 + 26);
+
+        for (int i = 0; i < 26; i++)
+            if (arr1[i] != arr2[i])
+                return false;
+
+        // if (word1 != word2)
+        // {
+
+        //     for (int i = 0; i < word1.size(); i++)
+        //         arr1[word1[i] - 'a']++, arr2[word2[i] - 'a']++;
+            
+        //     for (int i = 0; i < word1.size(); i++)
+        //         if (arr2[word1[i] - 'a'] == 0 || arr1[word2[i] - 'a'] == 0)
+        //             return false;
+        // }
+        return true;
+    }
+};
