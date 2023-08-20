10f7c992fd20f3681c395b0f8accae546050ecde
Abdeljalil Bouchfar <55533328+Abdeljalil-Bouchfar@users.noreply.github.com>
2023-08-20 12:06:56 +0100

    Time: 35 ms (86.78%) | Memory: 18.8 MB (82.07%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
-    bool ispossible(vector<int>& piles, int k, int h)
-    {
-        long th = 0;
-        for(int &p : piles)
-            th+= (p / k) + (p % k > 0);
-
-        return (th <= h);
-    }
-    
     int minEatingSpeed(vector<int>& piles, int h) {
         int s = 1, e = *max_element(piles.begin(), piles.end());
         while (s <= e)
         {
             int mid = s + (e - s) / 2;
-            if (ispossible(piles, mid, h)) e = mid - 1;
+            
+            long th = 0;
+            for(int &p : piles)
+                th+= (p / mid) + (p % mid > 0);
+
+            if (th <= h) e = mid - 1;
             else s = mid + 1;
         }
         return s;
