1954fa8158cd26ea87119d88c67d9bfb0b493bfe
Abdeljalil Bouchfar <55533328+Abdeljalil-Bouchfar@users.noreply.github.com>
2023-08-23 12:12:15 +0100

    Time: 62 ms (5.81%) | Memory: 26.6 MB (6.57%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    int n;
+    void dfs(vector<int>& candidates, vector<int> comps, int &tr, int i, int total)
+    {
+        if (total == tr) ans.push_back(comps);
+        else if (total < tr)
+        {
+            while (i < n)
+            {
+                vector<int> tmp = comps;
+                tmp.push_back(candidates[i]);
+                dfs(candidates, tmp, tr, i + 1, total + candidates[i]);
+                while(i < n && tmp.back() == candidates[i])
+                    i++;
+            }
+        }
+    }
+
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        n = candidates.size();
+        sort(candidates.begin(), candidates.end());
+        dfs(candidates, {}, target, 0, 0);
+        return ans;
+    }
+};
