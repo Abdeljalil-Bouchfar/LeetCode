21751d6fe7da0f3259b7b0109abff5cac447b82b
Abdeljalil Bouchfar <55533328+Abdeljalil-Bouchfar@users.noreply.github.com>
2023-08-31 22:13:48 +0100

    Time: 0 ms (100.00%) | Memory: 8.5 MB (89.03%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -2,20 +2,23 @@ class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size();
+        int hash[10001] = {0};
         vector<int> ans(n, -1);
+
+        for(int j = 0; j < nums2.size(); ++j)
+            hash[nums2[j]] = j + 1;
+        
         for(int j = 0; j < n; ++j)
         {
-            int i = 0;
-            while (i < nums2.size() && nums2[i] != nums1[j])
-                ++i;
-            while (i < nums2.size())
+            if (!hash[nums1[j]])
+                continue;
+            for (int i = hash[nums1[j]]; i < nums2.size(); ++i)
             {
                 if (nums2[i] > nums1[j])
                 {
                     ans[j] = nums2[i];
                     break;
                 }
-                ++i;
             }
         }
         return ans;
