883a2e05cf3cedad3e523adf034a8455c53bb862
Abdeljalil Bouchfar <55533328+Abdeljalil-Bouchfar@users.noreply.github.com>
2023-08-31 15:30:47 +0100

    Time: 341 ms (85.09%) | Memory: 89.8 MB (59.07%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.cpp
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.cpp
@@ -6,13 +6,12 @@ public:
         {
             return v1[1] < v2[1];
         });
+        
         int min = points[0][1];
-        // cout << points[0][0] << ',' << points[0][1] << endl;
         for (int i = 0; i < points.size(); ++i)
         {
             if (points[i][0] > min)
                 ans++, min = points[i][1];
-            // cout << points[i][0] << ',' << points[i][1] << endl;
         }
         return ans;
     }
