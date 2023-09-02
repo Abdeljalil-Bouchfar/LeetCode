2ed16d649dd94f42bfdf8b81d51f8228da246f0c
Abdeljalil Bouchfar <55533328+Abdeljalil-Bouchfar@users.noreply.github.com>
2023-09-02 17:51:06 +0100

    Time: 33 ms (88.97%) | Memory: 26.1 MB (89.85%) - LeetSync

--- 1397-search-suggestions-system/search-suggestions-system.cpp
+++ 1397-search-suggestions-system/search-suggestions-system.cpp
@@ -1,55 +1,7 @@
-// struct Node
-// {
-//     bool isWord = false;
-//     string word;
-//     Node* childs[26] = {0};
-// };
-
-// void fillTrie(vector<string>& products, Node *root)
-// {
-//     Node *tmp = root;
-//     for (auto &v : products)
-//     {
-//         for (auto &s : v)
-//         {
-//             tmp = root;
-//             for (auto &c : s)
-//             {
-//                 if (!tmp->childs[c - 'a'])
-//                     tmp->childs[c - 'a'] = new Node;
-//                 tmp = tmp->childs[c - 'a'];
-//                 tmp.word += c;
-//             }
-//             tmp->isWord = true;
-//         }
-//     }
-// }
-
-
-
 class Solution {
 public:
     vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
-        // Node *root = new Node;
-        // fillTrie(products, root);
-
         vector<vector<string>> ans;
-        // Node *curr = root, *tmp = nullptr;
-        // for (char &c : searchWord)
-        // {
-        //     curr = curr->childs[c - 'a'];
-        //     ans.push_back({});
-
-        //     if (curr)
-        //     {
-        //         tmp = curr;
-        //         while (tmp && ans.back().size() < 3)
-        //         {
-        //             if (tmp->isWord)
-        //                 ans.back().push_back(tmp->word);
-        //         }
-        //     }
-        // }
         sort(products.begin(), products.end());
         string curr;
         for (int j = 0, i= 0; j < searchWord.size(); ++j)
@@ -63,7 +15,7 @@ public:
                     ans.back().push_back(products[i++]);
                 else
                     break;
-            }                
+            }               
         }
         return ans;
     }
